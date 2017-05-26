6247ceb90b0b98f36732bfd386807cf4530af491
Leandro Silva <lsilva@tremorvideo.com>
2017-05-26 16:35:55 -0400

    Add custom modal
    - Clean up logic.
    - Refactor where needed.

--- src/trello-list-lister.js
+++ src/trello-list-lister.js
@@ -1,4 +1,11 @@
-//jshint esversion: 5, browser: true, jquery: true
+/**
+ * @author Leandro Silva
+ * @copyright 2014, 2017 Leandro Silva (http://grafluxe.com)
+ * @license MIT
+ */
+
+/* global TrelloListLister*/
+// jshint esversion: 5, browser: true, jquery: true
 
 (function () {
   "use strict";
@@ -7,40 +14,110 @@
       $trelloLists = $(".list");
 
   if ($listerBtns.length) {
-    if ($trelloLists.length === window.ListLister.$lists.length) {
-      return $listerBtns.toggle();
+    if ($trelloLists.length === TrelloListLister.$lists.length) {
+      $listerBtns.toggle();
+      if (TrelloListLister.$view) {
+        TrelloListLister.$view.hide();
+      }
+      return;
     } else {
       $listerBtns.remove();
+      TrelloListLister.$view.remove();
+      TrelloListLister.$view = null;
     }
   }
 
-  window.ListLister = {
+  window.TrelloListLister = {
+    version: "1.0.0",
+
     init: function () {
-      var btnHTML = "";
+      var $btn = $("<button>").addClass("trello-list-lister").text("List Cards").css({
+        margin: "5px",
+        width: "96%",
+        fontSize: "12px",
+        fontWeight: "500",
+        minHeight: "22px",
+        padding: "0",
+        opacity: "0.8"
+      }).click($.proxy(this.getData, this));
 
       this.$lists = $trelloLists;
 
-      btnHTML += "<button class='trello-list-lister' ";
-      btnHTML += "style='margin: 5px auto; width: 96%; font-size: 12px; font-weight: 500; min-height: 22px; padding: 0; opacity: 0.8;' ";
-      btnHTML += "onclick='ListLister.getData(this);'>List Data</button>";
-
-      $trelloLists.each($.proxy(function (i, el) {
+      $trelloLists.each(function (i, el) {
         if (i < $trelloLists.length) {
-          $(el).prepend(btnHTML);
+          $(el).prepend($btn.clone(true));
+        }
+      });
+    },
+
+    getData: function (evt) {
+      var out = "",
+          count = 0,
+          $listParent = $(evt.target).parent(),
+          $listCards = $listParent.find(".list-cards").children();
+
+      $listCards.each(function (i) {
+        count++;
+        out += "- " + $(this).find(".list-card-title").eq(0).contents().eq(1).text() + (i < $listCards.length - 1 ? "&#10;" : "");
+      });
+
+      if (!this.$view) {
+        this.createView();
+        this.createModal();
+      } else {
+        this.$view.show();
+      }
+
+      this.fillModal(
+        $listParent.find(".list-header-name-assist").text(),
+        count,
+        out
+      );
+    },
+
+    createView: function () {
+      this.$view = $("<div>").addClass("window-overlay").css("display", "block").appendTo("body").click($.proxy(function (evt) {
+        if (evt.target.className === "window-overlay") {
+          this.$view.hide();
         }
       }, this));
     },
 
-    getData: function (trelloList) {
-      var out = "";
+    createModal: function () {
+      var $modal = $("<div>").css({
+        position: "fixed",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: "75%",
+        maxWidth: "600px",
+        height: "75%",
+        backgroundColor: "#E2E4E6",
+        zIndex: 99,
+        borderRadius: "3px",
+        boxShadow: "0 0 20px #333",
+        padding: "15px"
+      }).appendTo(this.$view);
 
-      $(trelloList).parent().find(".list-cards").children().each(function () {
-        out += "- " + $(this).find(".list-card-title")[0].lastChild.textContent + "\n";
-      });
+      $("<a href='#'>").appendTo($modal).addClass("icon-lg icon-close dialog-close-button").click($.proxy(function () {
+        this.$view.hide();
+      }, this));
 
-      alert(out);
+      this.$header = $("<h2>").appendTo($modal);
+      this.$count = $("<p>").appendTo($modal);
+      this.$items = $("<textarea>").appendTo($modal).css({
+        height: "calc(100% - 70px)",
+        resize: "none",
+        padding: "5px 10px"
+      }).addClass("comment-frame");
+    },
+
+    fillModal: function (header, count, list) {
+      this.$header.text(header);
+      this.$count.text(count + " card" + (count !== 1 ? "s" : ""));
+      this.$items.html(list);
     }
   };
 
-  window.ListLister.init();
+  window.TrelloListLister.init();
 }());
